aa2b94f2ce9ba3af58c8c9044c6e0013771ebbcb
Adrien <napalm_ground@hotmail.com>
2020-11-13 16:32:51 +0100

    Spherical coordinates orientation

---
+++ Assets/Scripts/SphericalCoordinatesOrientation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphericalCoordinatesOrientation : MonoBehaviour
+{
+	[Header("Spherical Coordinates ρ θ φ")]
+	[SerializeField]
+	private float rayon;    //	radius
+	[SerializeField]
+	private float longitude;    //	elevation
+	[SerializeField]
+	private float colatitude;   //	polar
+
+	[Header("Quaternion angle")]
+	[SerializeField]
+	private float m_Angle;
+
+	void Start()
+	{
+	}
+
+	// Use this for initialization
+	void Update()
+	{
+		transform.rotation = Quaternion.AngleAxis(m_Angle, SphericalToCartesian(DegreesToRadiants(rayon), DegreesToRadiants(colatitude), DegreesToRadiants(longitude)));
+	}
+
+	private Vector3 SphericalToCartesian(float radius, float polar, float elevation)
+	{
+		Vector3 outCart = new Vector3();
+
+		float a = radius * Mathf.Cos(elevation);
+		outCart.x = a * Mathf.Cos(polar);
+		outCart.y = radius * Mathf.Sin(elevation);
+		outCart.z = a * Mathf.Sin(polar);
+
+		return outCart;
+	}
+
+	private float DegreesToRadiants(float degrees)
+	{
+		float radiants;
+
+		radiants = Convert.ToSingle(Math.PI * degrees / 180.0);
+
+		return radiants;
+	}
+}
